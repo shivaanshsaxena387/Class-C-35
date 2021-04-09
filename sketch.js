b852aa46a9336ea2e9a93a162d4e57e24cd09e39
shivaanshsaxena387 <73224094+shivaanshsaxena387@users.noreply.github.com>
2021-04-09 16:45:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,45 @@
+var car;
+var database,position;
+
+function setup(){
+    database=firebase.database()
+    createCanvas(500,500);
+    car = createSprite(250,250,20,60);
+    car.shapeColor = "green";
+
+    var carposition=database.ref('ball/position');
+    carposition.on("value",readPosition,showError);
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+     
+function writePosition(x,y){
+    database.ref('ball/position').set({
+    'x':position.x+x,
+    'y':position.y+y
+    })
+}
+function readPosition(data){
+position=data.val();
+car.x=position.x;
+car.y=position.y;
+}
+
+function showError(){
+    console.log("error in writing to the database")
+}
